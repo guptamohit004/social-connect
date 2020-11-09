899b68009a288ce0f43788c2c502a7931a03d628
Mohit Gupta <mohit.gupta30000@gmail.com>
2020-11-09 22:53:32 +0530

    circle ci added

--- public/sw.js
+++ public/sw.js
@@ -118,7 +118,7 @@ define("./sw.js",['./workbox-b90066a8'], function (workbox) { 'use strict';
     "revision": "0620b7c58e0f6f6643d1dd663a87864c"
   }, {
     "url": "/_next/static/runtime/webpack.js",
-    "revision": "6c101596560f6081313a597cd7dae783"
+    "revision": "8df0c861554b6fbf1ed4a9bfe88c5ab2"
   }], {
     "ignoreURLParametersMatching": [/ts/]
   });
